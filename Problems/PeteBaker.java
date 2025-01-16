11da74182fe2bbd675790d55e25e8d424f97605a
YungLust <artyr.yanchuk2@gmail.com>
2025-01-16 18:33:22 +0200

    6 kyu

---
+++ Problems/PeteBaker.java
@@ -0,0 +1,27 @@
+package Problems;
+
+import java.util.*;
+
+public class PeteBaker {
+    public static int cakes(Map<String, Integer> recipe, Map<String, Integer> available) {
+        List<Integer> result = new ArrayList<>();
+
+        Set<String> recipeProducts = recipe.keySet();
+        for (String product : recipeProducts) {
+            if (available.containsKey(product)) {
+                int availableAmount = available.get(product) / recipe.get(product);
+                //System.out.println(available.get(product)+" / "+ recipe.get(product) +" = "+availableAmount);
+                result.add(availableAmount);
+            } else {
+                return 0;
+            }
+        }
+
+        //System.out.println(Arrays.toString(result.toArray()));
+
+        Optional<Integer> cakesAmount = result.stream().min(Integer::compare);
+
+        // I think this check can be redundant, but it would prevent from unexpected errors
+        return cakesAmount.orElse(0);
+    }
+}
